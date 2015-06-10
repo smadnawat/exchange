981e4b3447f656443a2a10c49d99dafba8511970
Javed43 <javed.hussain@mobiloitte.com>
2015-06-10 18:57:04 +0530

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 # User.create([{email: 'rahul@mobiloitte.com', password: 'password', password_confirmation: 'password', name: 'Rahul', is_admin: 'false'},
 #            {email: 'admin@example.com', password: 'password', password_confirmation: 'password', name: 'Mark', is_admin: 'true'}
 #           ])
- 
+ AdminUser.create(email: 'a@test.com', password: '11111111', password_confirmation: '11111111')
  User.create(username: 'Testing name1', gender: 'Male', email: 'test1@gmail.com',password_digest: '11111111',location: 'Okhla',date_signup: ' Wed, 10 Jun 2015 13:01:42 +0000',created_at:' Wed, 10 Jun 2015 13:01:42 +0000',updated_at: ' Wed, 10 Jun 2015 13:01:42 +0000',latitude: '4.77',longitude: '5.277', provider: 'FB', u_id: 'sdsdsd')
  User.create(username: 'Testing name2', gender: 'Male', email: 'test2@gmail.com',password_digest: '11111111',location: 'Govindpuri',date_signup: ' Wed, 10 Jun 2015 13:01:42 +0000',created_at:' Wed, 10 Jun 2015 13:01:42 +0000',updated_at: ' Wed, 10 Jun 2015 13:01:42 +0000',latitude: '4.747',longitude: '35.77', provider: 'FB', u_id: 'sdsdeesd')
  User.create(username: 'Testing name3', gender: 'Male', email: 'test3@gmail.com',password_digest: '11111111',location: 'Mayur Vihar',date_signup: ' Wed, 10 Jun 2015 13:01:42 +0000',created_at:' Wed, 10 Jun 2015 13:01:42 +0000',updated_at: ' Wed, 10 Jun 2015 13:01:42 +0000',latitude: '4.737',longitude: '55.77', provider: 'FB', u_id: 'sdseedsd')
