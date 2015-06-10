7d722040fe05a1a93f9d5a61b0d3f97223ac3d80
Javed43 <javed.hussain@mobiloitte.com>
2015-06-10 15:15:39 +0530

    push on heriku

--- app/controllers/banners_controller.rb
+++ app/controllers/banners_controller.rb
@@ -1,6 +1,6 @@
 class BannersController < ApplicationController
 	def destroy
-		p"jdksjdkjskdjksjdkjskdjksjdksjd"
+		
        banner = Banner.find(params[:id])
        if banner.destroy
          flash[:notice]= 'Banner has been deleted..'
