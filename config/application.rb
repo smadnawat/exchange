890309863cd3e30ad9695a18ebf83cf0292075fb
Javed43 <javed.hussain@mobiloitte.com>
2015-06-11 14:12:36 +0530

    heroku push

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module ExchangeApp
     # -- all .rb files in that directory are automatically loaded.
     # config.assets.paths << Rails.root.join('vendor', 'assets')
     config.serve_static_files = true 
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
