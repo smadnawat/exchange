83971517df3c7675a15b9bbb5dd3bf9511ecf57f
Rahul Pakhre <rahul.pakhre@mobiloittegroup.com>
2015-09-22 20:31:47 +0530

    adminuser removed

--- app/workers/bunching_notification_worker.rb
+++ app/workers/bunching_notification_worker.rb
@@ -14,9 +14,10 @@ class BunchingNotificationWorker
       groups.each do |gr|
         users = gr.users.where(id: gr.user_groups.where('is_deleted is null or is_deleted =?',false).pluck(:user_id))
           users.each do |user|
+
             break if Block.find_by_user_id_and_group_id(user.id, gr.id).present?
             msg  = @messages.select{|m|(m[:sender_id]!=user.id and m[:group_id] == gr.id)}
-              user.devices.each {|device| (device.device_type == "Android") ? AndroidPushWorker.perform_async(nil, "You have #{msg.count} unread messages in #{gr.name} ", nil, device.device_id, "message", nil, gr.id, nil) : ApplePushWorker.perform_async(nil, "You have #{msg.count} unread messages in #{gr.name} ", nil, device.device_id, "message", nil, gr.id, nil) }    
+              user.devices.each {|device| (device.device_type == "Android") ? AndroidPushWorker.perform_async(nil, "You have #{msg.count} unread messages in #{gr.name} ", nil, device.device_id, "message", nil, gr.id, nil) : ApplePushWorker.perform_async(nil, "You have #{msg.count} unread messages in #{gr.name} ", nil, device.device_id, "message", nil, gr.id, nil) }  if (@messages.pluck(:sender_id).uniq.count > 1) and (@messages.first.sender_id.eql?(user.id))
               gr.messages.update_all(:is_send => true)
           end
       end
