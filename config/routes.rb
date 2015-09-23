686ec5857576ebbc05853effcdad0a3a1c77a58c
Rahul Pakhre <rahul.pakhre@mobiloittegroup.com>
2015-09-23 18:50:14 +0530

    logout routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ Rails.application.routes.draw do
 ############### web services API controller###########################
   post '/register' =>   'apis#register', as: :register
   post '/login' =>   'sessions#create', as: :login
-  delete '/logout' =>   'sessions#destroy', as: :logout
+  post '/logout' =>   'sessions#destroy', as: :logout
   post '/upload_books'  => 'apis#upload_books', as: :upload_books
   get  '/get_uploaded_books' => 'apis#get_uploaded_books', as: :get_uploaded_books
   post  '/upload_reading_preferences' => 'apis#upload_reading_preferences', as: :upload_reading_preferences
