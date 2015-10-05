0fc94582ccf52d520468565787fcefd471273927
Rahul Pakhre <rahul.pakhre@mobiloittegroup.com>
2015-10-05 15:50:34 +0530

    clock.rb changes

--- lib/clock.rb
+++ lib/clock.rb
@@ -5,7 +5,7 @@ include Clockwork
 
 every(1.week,'weekly_worker called', :at => 'Sunday 13:05', :tz => 'UTC') { WeeklyWorker.perform_async }
 
-every(1.day,'monthly worker called', :at => '08:55', :tz => 'UTC') { MonthlyEmailWorker.perform_async }
+every(1.day,'monthly worker called', :at => '10:26', :tz => 'UTC') { MonthlyEmailWorker.perform_async }
 
 # every(1.day, 'monthly worker called', :if => lambda { |t| t.day == 1 } ) { MonthlyEmailWorker.perform_async }
 
