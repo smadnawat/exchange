3212b34c137c9e5c0a74020240d17056ff7398dd
Rahul Pakhre <rahul.pakhre@mobiloittegroup.com>
2015-11-18 19:34:41 +0530

    changes done for scheduling the newsletter and weekly notification

--- lib/clock.rb
+++ lib/clock.rb
@@ -3,11 +3,14 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'clockwork'
 include Clockwork
 
-# every(1.week,'weekly_worker called', :at => 'Sunday 13:05', :tz => 'UTC') { WeeklyWorker.perform_async }
+every(1.week,'weekly_worker called', :at => 'Thursday 10:00', :tz => 'UTC') { WeeklyWorker.perform_async }
 
-every(1.day,'weekly_worker called', :at => '09:50', :tz => 'UTC') { WeeklyWorker.perform_async }
+# every(1.day,'weekly_worker called', :at => '09:50', :tz => 'UTC') { WeeklyWorker.perform_async }
 
-every(1.day,'monthly worker called', :at => '11:45', :tz => 'UTC') { MonthlyEmailWorker.perform_async }
+# every(1.day,'monthly worker called', :at => '11:45', :tz => 'UTC') { MonthlyEmailWorker.perform_async }
+
+every(1.day,'monthly worker called', :at => '14:00', :tz => 'UTC',:if => lambda { |t| ((1..7).to_a.include?(t.day) and t.wday==4)}) { MonthlyEmailWorker.perform_async }
+
 
 # every(1.day, 'monthly worker called', :if => lambda { |t| t.day == 1 } ) { MonthlyEmailWorker.perform_async }
 
