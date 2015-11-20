5a8ef865f61fb697235b957eb8ce09ce6325b0bc
Arvind Kushwah <arvind_kushwah@seologistics.com>
2015-11-20 18:57:34 +0530

    comment add_index_to_book migration

--- db/migrate/20150626135123_add_index_to_book.rb
+++ db/migrate/20150626135123_add_index_to_book.rb
@@ -1,7 +1,7 @@
 class AddIndexToBook < ActiveRecord::Migration
   def change
   	#add_index "book", "subjects", name: "subjects", using: :btree
-  	add_index "book", "title", name: "title", length: { "title" => 10 }, using: :btree
-  	add_index "book", "author", name: "author", using: :btree
+  	# add_index "book", "title", name: "title", length: { "title" => 10 }, using: :btree
+  	# add_index "book", "author", name: "author", using: :btree
   end
 end
