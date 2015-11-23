33aa1b7d8617d54d8da8d6985d53358556386c3d
Hemant Gupta <hemant_gupta@geologistics.com>
2015-11-23 10:13:58 +0530

    shrik popop for all screen

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,10 +1,10 @@
 $(document).on("ready", function(){
-	if($(window).width > 767)
-	{
+	// if($(window).width > 767)
+	// {
 		$(".navbar-nav > li > a").click(function(){
-			$("#bs-example-navbar-collapse-1").removeClass("in");										 
+			$("#bs-example-navbar-collapse-1").removeClass("in");
 		});
-	}		
+	// }
 	$(".btn-red2").click(function(){
 		$("body").toggleClass("bdyfixed");
 	});
